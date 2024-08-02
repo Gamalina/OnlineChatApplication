21ee933fa16a34256cac49a2fe19ee819bce5fc6
Daniel Rud Callesen <Danielrudcallesen96@gmail.com>
2024-08-02 17:23:41 +0200

    Added EmailService

---
+++ OnlineChatApplication/MailService/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace OnlineChatApplication.MailService
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("Online Chat Application", emailSettings["FromEmail"]));
+            emailMessage.To.Add(new MailboxAddress("", toEmail));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(emailSettings["SmtpServer"], int.Parse(emailSettings["SmtpPort"]), MailKit.Security.SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync(emailSettings["SmtpUser"], emailSettings["SmtpPass"]);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}

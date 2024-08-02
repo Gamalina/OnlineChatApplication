2fe265315ebfae35aabc85846237f430bc2f9f21
Daniel Rud Callesen <Danielrudcallesen96@gmail.com>
2024-08-02 16:53:28 +0200

    Added User Model

---
+++ OnlineChatApplication/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineChatApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+    }
+}
